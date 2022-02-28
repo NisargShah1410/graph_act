5d33affc6e0fc4eb5529337c9e2042ef5d43252c
NisargShah1410 <nisargshah01410@gmail.com>
2022-02-28 06:22:53 +0000

    first

---
+++ index.js
@@ -0,0 +1,106 @@
+import fetch from "node-fetch";
+import fs from "fs";
+const { exec, execFile } = require('child_process');
+const core = require('@actions/core');
+const github = require('@actions/github');
+var child_process = require('child_process');
+
+try {
+
+    const openSource = {
+        githubConvertedToken: "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",
+        githubUserName: "NisargShah1410",
+      };
+      
+      //const fetch = require("node-fetch");
+      //const fetch = require('node-fetch');
+      //var fs = require("fs");
+      //var fs = require('fs');
+      
+      const query_pr = {
+        query: `
+          query {
+            user(login: "${openSource.githubUserName}"){
+              pullRequests(last: 100, orderBy: {field: CREATED_AT, direction: DESC}){
+            totalCount
+            nodes{
+              id
+              title
+              url
+              state
+                mergedBy {
+                    avatarUrl
+                    url
+                    login
+                }
+                createdAt
+                number
+              changedFiles
+                additions
+                deletions
+              baseRepository {
+                    name
+                    url
+                    owner {
+                      avatarUrl
+                      login
+                      url
+                    }
+                  }
+            }
+          }
+          }
+      }
+          `,
+      };
+      
+      const baseUrl = "https://api.github.com/graphql";
+    
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: "bearer " + openSource.githubConvertedToken,
+      };
+    
+    
+      fetch(baseUrl, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(query_pr),
+      })
+        .then((response) => response.text())
+        .then((txt) => {
+          const data = JSON.parse(txt);
+          var cropped = { data: [] };
+          cropped["data"] = data["data"]["user"]["pullRequests"]["nodes"];
+      
+          var open = 0;
+          var closed = 0;
+          var merged = 0;
+          for (var i = 0; i < cropped["data"].length; i++) {
+            if (cropped["data"][i]["state"] === "OPEN") open++;
+            else if (cropped["data"][i]["state"] === "MERGED") merged++;
+            else closed++;
+          }
+      
+          cropped["open"] = open;
+          cropped["closed"] = closed;
+          cropped["merged"] = merged;
+          cropped["totalCount"] = cropped["data"].length;
+      
+          console.log("Fetching the Pull Request Data.\n");
+          fs.writeFile(
+            "./pull_requests.json",
+            JSON.stringify(cropped),
+            function (err) {
+              if (err) {
+                console.log(err);
+              }
+            }
+          );
+        })
+        .catch((error) => console.log(JSON.stringify(error)));
+
+
+} catch (error) {
+  core.setFailed(error.message);
+}
